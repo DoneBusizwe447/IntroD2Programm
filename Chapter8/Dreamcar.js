433ad9bf3210032ec5676ecad71811b1d5f37d4b
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2023-03-03 19:23:35 +0200

    gold

---
+++ Chapter8/Dreamcar.js
@@ -0,0 +1,33 @@
+var dreamCar = {
+    make: "Porsche",
+    model: "98",
+    color: "brown",
+    year: 1997,
+    bodyStyle: "Luxury Car",
+    price: 97000,
+}
+
+document.getElementById("pricetag").innerHTML = dreamCar.price;
+document.getElementById("modelyear").innerHTML = dreamCar.year;
+document.getElementById("body").style.backgroundColor = dreamCar.
+color;
+document.getElementById("body").innerHTML = dreamCar.make + " " +
+    dreamCar.model;
+
+    alert.prompt("make:")
+    prompt("model:")
+    prompt("color:")
+    prompt("year")
+    prompt("currency in price of car")
+
+document.getElementById("pricetag")
+  .innerHTML = dreamCar.price;
+
+document.getElementById("model:")
+  .innerHTML = dreamCar.year;
+
+document.getElementById("body")
+  .style.backgroundColor = dreamCar.color;
+
+document.getElementById("body")
+  .innerHTML = dreamCar.make + " " + dreamCar.model;
